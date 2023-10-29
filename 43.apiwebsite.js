86923cd4bf4c7ec0b6ec63a9d2673925a7fdcdb5
sourabh23030303 <star.sourabh2002@gmail.com>
2023-10-29 18:30:25 +0530

    updated

--- 43.apiwebsite.js
+++ 43.apiwebsite.js
@@ -4,28 +4,28 @@
 let url = "https://kontests.net/api/v1/all"
 let response = fetch(url)
 response.then((v)=>{
-
+       
      return v.json()
-}).then((arrayhai)=>{
-    console.log(arrayhai)//fetched data is in array format with kesy(1,2,3 here) and value or vo value  object hai
+}).then((objecthai)=>{
+    console.log(objecthai)//fetched data is in array format with kesy(1,2,3 here) and value or vo value  object hai
    let ihtml = ""
-    for(item in arrayhai){ //item yha pe 0,1,2,3,4,5,..49 hai or uski value detail hai
-      console.log(arrayhai[item]) // values nikal li arrays ki
+    for(item in objecthai){ //item yha pe 0,1,2,3,4,5,..49 hai or uski value detail hai
+      console.log(objecthai[item]) // values nikal li arrays ki
            ihtml += ` <div class="card" style="width: 18rem;">
-          <img src=${image[randomImage]}>
+          <img src=${image[Math.floor(Math.random() * image.length)]}>
            <div class="card-body">
-             <h5 class="card-title">${arrayhai[item].name}</h5> 
-             <h5 class="card-title">starts at ${arrayhai[item].start_time}</h5>
-             <h5 class="card-title">ends on ${arrayhai[item].end_time}</h5>
-             <h5 class="card-title">site name: ${arrayhai[item].site}</h5>
+             <h5 class="card-title">${objecthai[item].name}</h5> 
+             <h5 class="card-title">starts at ${objecthai[item].start_time}</h5>
+             <h5 class="card-title">ends on ${objecthai[item].end_time}</h5>
+             <h5 class="card-title">site name: ${objecthai[item].site}</h5>
              
-             <a href="${arrayhai[item].url}" class="btn btn-primary">go to participate</a>
+             <a href="${objecthai[item].url}" class="btn btn-primary">go to participate</a>
            </div>
          </div>
            `                           
 
+           cardcontainer.innerHTML = ihtml//card container mai inner html karo jiska name ihtml hai or i html mai cards ka loop lyaga hai
         }
-        cardcontainer.innerHTML = ihtml//card container mai inner html karo jiska name ihtml hai or i html mai cards ka loop lyaga hai
 })
 
 //yha pe mene cardcontainer id mai call card likha hai
